5d36f87c87b03c7b06983ca4b84a0eaf77b08bc0
Andre Bossard <abossard@users.noreply.github.com>
2025-06-24 17:51:09 +0200

    feat: add phase editor for comments

--- database/sqlite/schema/library-settings.ts
+++ database/sqlite/schema/library-settings.ts
@@ -9,7 +9,8 @@ const client = db();
 client.exec(`
   CREATE TABLE IF NOT EXISTS library_settings (
     id INTEGER PRIMARY KEY CHECK (id = 1),
-    base_folder TEXT
+    base_folder TEXT,
+    phases TEXT
   );
 `);
 
@@ -21,4 +22,14 @@ export const setBaseFolder = `
 
 export const getBaseFolder = `
   SELECT base_folder FROM library_settings WHERE id = 1;
+`;
+
+export const setPhases = `
+  INSERT INTO library_settings (id, phases)
+  VALUES (1, ?)
+  ON CONFLICT(id) DO UPDATE SET phases = excluded.phases;
+`;
+
+export const getPhases = `
+  SELECT phases FROM library_settings WHERE id = 1;
 `;
